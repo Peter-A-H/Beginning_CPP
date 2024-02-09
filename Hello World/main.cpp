91600ed11d1ff1a7e954e80ad2dbd3fe52fa154b
Peter <peterhamblin@hotmail.co.uk>
2024-02-09 20:27:51 +0000

    Added Lost Fortune small text adventure

--- Hello World/main.cpp
+++ Hello World/main.cpp
@@ -1,9 +1,46 @@
 #include <iostream>
 using std::cout;
+using std::cin;
 using std::endl;
+using std::string;
 
 int main()
 {
-    cout << "Hello World!" << endl;
+    const int GOLD_PIECES = 900;
+    int adventurers = 0;
+    int killed = 0;
+    int survivors = 0;
+    string leader = "";
+
+    cout << "Welcome to Lost Fortune\n\n";
+    cout << "Please enter the following for your personalized adventure\n";
+    
+    cout << "Please enter a number: ";
+    cin >> adventurers;
+
+    cout << "Enter a number, smaller than the first: ";
+    cin >> killed;
+
+    survivors = adventurers - killed;
+
+    cout << "Enter your last name: ";
+    cin >> leader;
+
+    cout << "\nA brave group of " << adventurers << " set out on a quest ";
+    cout << "-- in search of the lost treasure of the Ancient Dwarves. ";
+    cout << "The group was led by the legendary rogue, " << leader << ".\n";
+    cout << "\nAlong the way, a band of marauding ogres ambushed the party. ";
+    cout << "All fought bravely under the command of " << leader;
+    cout << ", and the ogres were defeated, but at a cost. ";
+    cout << "Of the adventurers, " << killed << " were vanquished, ";
+    cout << "leaving just " << survivors << " in the group.\n";
+
+    cout << "\nThe party was about to give up all hope. ";
+    cout << "But while laying the deceased to rest, ";
+    cout << "they stumbled upon the buried fortune. ";
+    cout << "So the adventurers split " << GOLD_PIECES << " gold pieces. ";
+    cout << leader << " held on to the extra " << (GOLD_PIECES % survivors);
+    cout << " pieces to keep things fair of course.\n";
+
     return 0;
 }
